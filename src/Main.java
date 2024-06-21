9454c5be32459ca90a98a5d137c8ae89c3715dc5
MonicaVelasquez <monicam.velasquezm@uqvirtual.edu.co>
2024-06-20 20:55:15 -0500

    Se crea proyecto base

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static int suma(int numero1,int numero2){
+        return 0;
+    }
+
+    
+    public static int resta(int numero1,int numero2){
+        return 0;
+    }
+
+    public static int multiplicacion(int numero1,int numero2){
+        return 0;
+    }
+
+    public static double division(int numero1,int numero2){
+        return 0;
+    }
+}

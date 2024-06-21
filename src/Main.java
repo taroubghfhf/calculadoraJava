dff169c43cff63326ee4e5475647526f52067b33
MonicaVelasquez <monicam.velasquezm@uqvirtual.edu.co>
2024-06-20 21:09:33 -0500

    Se crea método de suma

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
 
+        int resultado=suma(1,2);
+        System.out.println("este es el resultado:"+resultado);
+    }
+  
     public static int suma(int numero1,int numero2){
-        return 0;
+        int total = numero1 + numero2;
+        return total;
     }
 
-    
     public static int resta(int numero1,int numero2){
         return 0;
     }
